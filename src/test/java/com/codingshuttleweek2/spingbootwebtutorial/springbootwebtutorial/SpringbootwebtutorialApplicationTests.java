9d2e4427243f6841328b20840c85da732555d790
Snehajit Dey <snehajitdey2602@gmail.com>
2025-11-29 17:01:13 +0530

    Chapter 2.1

---
+++ src/test/java/com/codingshuttleweek2/spingbootwebtutorial/springbootwebtutorial/SpringbootwebtutorialApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codingshuttleweek2.spingbootwebtutorial.springbootwebtutorial;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootwebtutorialApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

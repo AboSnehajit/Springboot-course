a7979756ebb0f1e9a1de9b3b4463023561264505
Snehajit Dey <snehajitdey2602@gmail.com>
2025-11-29 20:44:53 +0530

    resolved merge conflicts

---
+++ src/test/java/com/codingshuttle/snehajit/module1Intrroduction/Module1IntrroductionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codingshuttle.snehajit.module1Intrroduction;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Module1IntrroductionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
